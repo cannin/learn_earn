f4f706cad2276dd274d1b389a4a8641360adceac
cannin <cannin@gmail.com>
2024-05-25 11:41:14 -0400

    Loop through available datasets

--- src/pages/games.js
+++ src/pages/games.js
@@ -4,46 +4,20 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import {Seo} from "../components/seo"
 
+import config from "../../site-config"
+
 const GamesPage = () => (
   <Layout>
     <div className="container my-5">
       <h1 className="text-center">Learn & Earn: Choose Game</h1>
       <ul>
-        <li>
-          <Link
-            to="/game/animals"
-          >
-            Animals
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/game/estados"
-          >
-            Capitales de Estados de Mexico
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/game/multiplication_10x"
-          >
-            Multiplication: 10
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/game/multiplication_20x"
-          >
-            Multiplication: 20
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/game/us_capitals"
-          >
-            US State Capitals
-          </Link>
-        </li>
+        {Object.entries(config.sets).map(([key, value]) => (
+          <li key={key}>
+            <Link to={`/game/{key}`}>
+              {value}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   </Layout>
